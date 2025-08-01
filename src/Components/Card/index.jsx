9620a7a8f30ed0c8ef97760f3d3966731e78a787
WarCrazhed <mariozamora5126@gmail.com>
2025-08-01 08:23:42 -0600

    isProductDetailOpen

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -4,10 +4,15 @@ import { ShoppingCartContext } from "../../Context";
 
 const Card = ({data}) => {
     const {title, price, images, category: {name}} = data;
-    const {count, setCount} = useContext(ShoppingCartContext);
+    const {count, setCount, openProductDetail} = useContext(ShoppingCartContext);
 
     return (
-        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+        <div
+            onClick={() => {
+                openProductDetail();
+            }}
+            className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+        >
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{name}</span>
                 <img 
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context';
 
 const ProductDetail = () => {
+    const { isProductDetailOpen, closeProductDetail } = useContext(ShoppingCartContext);
     return (
-        <aside className="top-[68px] w-[360px] h-[calc(100vh-68px)] flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+        <aside
+            className={isProductDetailOpen ? "top-[68px] w-[360px] h-[calc(100vh-68px)] flex flex-col fixed right-0 border border-black rounded-lg bg-white" : "hidden"}
+        >
             <div className="flex justify-between items-center p-6">
                 <h2 className="text-xl font-medium">Detail</h2>
                 <button
+                    onClick={() => {
+                        closeProductDetail();
+                    }}
                     className="w-6 h-6 cursor-pointer text-black hover:text-black/80"
                 >
                     <XMarkIcon className="size-6" />
